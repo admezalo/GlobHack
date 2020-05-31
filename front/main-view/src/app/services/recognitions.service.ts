4368d6b53f9fd333d65ce4b328cf16df7c8afd1a
Servi Repuestos Desk <jmaluchesa@outlook.com>
2020-05-30 21:37:58 -0500

    create speach to text service

---
+++ front/main-view/src/app/services/recognitions.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { getSpeechRecognition, getSpeechGrammarList, getSpeechRecognitionEvent, geSpeechSynthesis } from './testSpeech';
+
+const SpeechRecognition: any = getSpeechRecognition();
+const SpeechGrammarList: any = getSpeechGrammarList();
+const SpeechRecognitionEvent: any = getSpeechRecognitionEvent();
+const speechSynthesis: any = geSpeechSynthesis();
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecognitionsService {
+
+
+  title = 'testConcept';
+  public words: Array<string> = [];
+  public speechSynthesis = speechSynthesis;
+  public speechRecognitionList = new SpeechGrammarList();
+  public recognition = new SpeechRecognition();
+  public wordListening: any;
+
+  constructor() {
+    this.words = ['deportes', 'actualidad', 'politica'];
+    var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + this.words.join(' | ') + ' ;'
+
+    this.speechRecognitionList.addFromString(grammar, 1);
+    this.recognition.grammars = this.speechRecognitionList;
+    this.recognition.continuous = false;
+    this.recognition.lang = 'es-CO';
+    this.recognition.interimResults = false;
+    this.recognition.maxAlternatives = 1;
+  }
+
+  getRecognitionInstance = () => this.recognition;
+
+  createSpeak = textToSpeach => {
+    var speech = new SpeechSynthesisUtterance();
+    var agnesIndex = this.speechSynthesis.getVoices().findIndex(function (voice) {
+      return voice.name === 'Agnes';
+    });
+
+    // Set the text and voice attributes.
+    speech.voice = this.speechSynthesis.getVoices()[agnesIndex];
+    speech.text = textToSpeach;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    this.speechSynthesis.speak(speech);
+  }
+}
