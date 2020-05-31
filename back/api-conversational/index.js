c3c26b94e574d89ddd00aa9831db4d270fbd17c6
Andres Sierra <dante.andres.swei@gmail.com>
2020-05-30 22:03:36 -0500

    Test

--- back/api-conversational/index.js
+++ back/api-conversational/index.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const assistantRouter = require('./routes/assistantRouter');
 
 /* configuration */
 app.set('port', process.env.PORT || 5000);
-app.set('json sapces', 2);
+app.set('json spaces', 2);
 
-/* middlewares */
+/* mongoose */
+//const URI = "mongodb+srv://gglobhack:<password>@cluster0-tzq0g.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('Connected to dataBase ' + mongoose.connection.client.s.url);
+    })
+    .catch(error => console.log(error.message))
+
+    /* middlewares */
 app.use(express.json());
 
 /* controllers */
